98578c6f3022a64e8d9374f87c89d35de04d975f
Natalie Lee <natalienatashalee@gmail.com>
2014-12-09 16:55:26 +0000

    Initial Homepage Style commit

---
+++ js/script.js
@@ -0,0 +1,74 @@
+$(document).ready(function() {
+
+  //mobile navigation
+  $("#nav_primary").mmenu({
+     // options
+  }, {
+     // configuration
+     clone: true,
+     offCanvas: {
+        pageNodetype: "nav"
+     }
+  });
+
+  //mobile navigation burger button
+  $('#nav_button').click(function(){
+    $('#nav_primary').trigger("open.mm");
+  });
+
+  $(function () {
+    $('.cycle-slideshow').maximage({
+        cycleOptions: {
+            fx:'fade',
+            speed: 4000,
+            timeout: 800,
+            prev: '.slider-left',
+            next: '.slider-right',
+            pager:  '.slider-nav'
+        },
+
+        onFirstImageLoaded: function(){
+          jQuery('.slider-content').hide();
+          jQuery('.slider-content').fadeIn('slow');
+        },
+
+        fillElement: '.home-slider',
+
+        onImagesLoaded: function() {
+           $('.testimonial-slideshow').maximage({
+               cycleOptions: {
+                   fx: 'fade',
+                   speed: 4000,
+                   timeout: 800,
+                   prev: '.testimonials-left',
+                   next: '.testimonials-right',
+                   pager:  '.testimonial-nav'
+                 },
+                 fillElement: '.testimonial-slider',
+              });
+          }
+      });
+  });
+
+  //fancybox popup for video
+  $('.video-link').fancybox({
+    maxWidth:800,
+    maxHeight:600,
+    fitToView:false,
+    width:'70%',
+    height:'70%',
+    autosize:false,
+    closeClick:false,
+    openEffect:'none',
+    closeEffect:'none',
+    padding:0
+  });
+
+  //scroll down button on slider
+  $(".icon-down-button").click(function(){
+    $('html, body').animate({
+      scrollTop: $("#expert-advice").offset().top
+    }, 1000);
+  });
+
+});
